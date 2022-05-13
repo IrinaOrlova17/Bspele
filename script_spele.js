9eb165244d441aa2be9f6fadd05f5bab6fd59df3
IrinaOrlova17 <93528037+IrinaOrlova17@users.noreply.github.com>
2022-05-13 19:49:42 +0300

    z

--- script_spele.js
+++ script_spele.js
@@ -26,12 +26,12 @@ function veiktGajienu(laukums)
   if(atvertsJaunsLaukums)
   {
   document.querySelector('#'+laukums+' div').style.display="block"
-      atvertieLaukumi.push(laukums)
+     pedejieDivi.push(laukums)
   }
   if(pedejieDivi.length == 2)
   {
-    let atverts1 = pedejieDivi[0].replase ('L','')
-    let atverts2 =pedejieDivi[1].replase ('L','')
+    let atverts1 = pedejieDivi[0].replace ('L','')
+    let atverts2 =pedejieDivi[1].replace ('L','')
     atverts1 = parseInt(atverts1,10)
     atverts2 = parseInt(atverts2,10)
     if(laukumsSaturs[atverts1] == laukumsSaturs[atverts2])
@@ -49,8 +49,8 @@ function veiktGajienu(laukums)
 
         setTimeout( function()
         {  
-            document.querySelector('#'+pedejieDiviCopy[0]+' div').style.display ='none'
-            document.querySelector('#'+pedejieDiviCopy[1]+' div').style.opacity='none'
+        document.querySelector('#'+pedejieDiviCopy[0]+' div').style.display ='none'
+        document.querySelector('#'+pedejieDiviCopy[1]+' div').style.opacity='none'
 
 
          }    ,500  )
